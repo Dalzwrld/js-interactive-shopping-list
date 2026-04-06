09792491fc78ae4124e2717a1ed891b94251dade
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-04-06 17:00:35 +0300

    feat: add initial project files

---
+++ main.js
@@ -0,0 +1,70 @@
+// Define the array
+let shoppingItems = [];
+
+// Fetch your elements from your HTML code
+const name = document.getElementById("itemName");    
+const price = document.getElementById("itemPrice");
+const addButton = document.querySelector("addButton");
+const displayList = document.getElementById("displayList");
+
+// Create a function to add your items
+function addItem(name, price) {
+    const name = itemName.value;
+    const price = itemPrice.value;
+
+    // Create an object to store each product with its price
+    const shoppingItemsObject = {
+        itemName: name,
+        itemPrice: Number(price),
+        isPurchased: false, 
+    };
+
+    // Add your object into your array
+    shoppingItems.push(shoppingItemsObject);
+
+    // Clear out your input fields
+    itemName.value = "";
+    itemPrice.value = "";
+
+}
+
+// Create a new function to display your items in your display list
+function displayItems() {
+    // Clear out your old display
+    displayList.innerHTML = "";
+
+    // Create a card that'll display your items after addition
+    shoppingItems.forEach(item => {
+        const card = document.createElement("div");
+        card.classList.add("card");
+
+        card.innerHTML = `
+            <h4>${item.name}</h4>
+            <p>${item.price}</p>
+            <label><input type="checkbox" id="purchase">${item.isPurchased}</label>
+        `;
+
+        // Create an event listener for marking your item as complete
+        card.querySelector("purchase").addEventListener("click", () => {
+            item.isPurchased = !item.isPurchased;
+            displayItems();
+        });
+    });
+}
+
+// Add a clear list button
+const clearButton = document.createElement("button");
+clearButton.classList.add("clearButton");
+
+clearButton.innerHTML
+
+// Check for validity of your inputs
+function checkInputType(name, price) {
+    if (typeof price !== number && typeof name !== string ) {
+        console.log("Input not valid");
+    } else {
+        console.log(`${key}: ${variable[key]}`);
+    }
+}
+
+addButton.addEventListener("click", addItem);
